5edcdd0da147a0cdef6405c37d7fda13bb7df9e9
Samuel John <github@samueljohn.de>
2015-12-11 16:38:25 +0100

    center_roi_around: fix naming hint
    row vs. column mixup. Updated doc string, too.

--- find_pattern_rotated.py
+++ find_pattern_rotated.py
@@ -3,31 +3,30 @@
 """
 
 
-def center_roi_around(center_cr, size_hw):
+
+def center_roi_around(center_rc, size_hw):
     """
     Return a rectangular region of interest (ROI) of size `size_hw` around the
     given center coordinates. The returned ROI is defined as
 
-        (start_column, start_row, end_column, end_row)
+        (start_row, start_column, end_row, end_column)
 
-    where the start_column/row are ment to be *included* and the
+    where the start_row/column are ment to be *included* and the
     end_row/column are excluded.
 
-    -   `center_cr`: Tuple of (column, row). The numbers will be rounded to the
-        closest integer.
-    -   `size_hw`: A tuple of (height, width) of the resulting ROI. If this
-        numbers are even
+    -   `center_rc`: Tuple of `(row, column)`. The numbers will be rounded to
+        the closest integer. The `row` corresponds to the height and the
+        `column` to the width. In mathematical notation you could think of the
+        center to be given as the tuple `(y, x)`. Be aware of this; It does
+        fit well to numpy's `shape` method.
+    -   `size_hw`: A tuple of `(height, width)` of the resulting ROI. If this
+        numbers are even, a UserWarning is issued.
     """
     height, width = size_hw
     if height % 2 == 0:
         raise UserWarning("ROI with even height is not centered.")
     if width % 2 == 0:
         raise UserWarning("ROI with even width is not centered.")
-    col, row = int(round(center_cr[0])), int(round(center_cr[1]))
-    return (col-height//2,
-            row-width//2,
-            1+col+height//2,
-            1+row+width//2)
 
 
 def test_center_roi_around():
@@ -36,3 +35,8 @@ def test_center_roi_around():
     assert np.allclose(center_roi_around((5, 5), (10, 10)), test_roi)
     test_roi = [0, 0, 1, 1]  # this is a 1x1 ROI
     assert np.allclose(center_roi_around((0, 0), (1, 1)), test_roi)
+    row, col = int(round(center_rc[0])), int(round(center_rc[1]))
+    return (row - height//2,
+            col - width//2,
+            row + height//2 + 1,
+            col + width//2 + 1)
